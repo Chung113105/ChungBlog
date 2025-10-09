5c7c2498c77c8028ca2a1e79cf3908559721089f
Chung113105 <BMA113105@gm.ntcu.edu.tw>
2025-10-09 22:09:35 +0800

    add cv/Resume

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,10 +1,10 @@
 export const experiences = [
 	{
-		company: '',
-		time: '',
-		title: '',
-		location: '',
-		description: '',
+		company: '彰化縣南郭國民小學',
+		time: '2023/08 - 2025/07',
+		title: '代理教師',
+		location: '彰化市, 台灣',
+		description: '級任導師。',
 	},
 	// {
 	// 	company: 'Radium Institute (Institut du Radium)',
@@ -17,11 +17,25 @@ export const experiences = [
 
 export const education = [
 	{
-		school: '',
-		time: '',
-		degree: '',
-		location: '',
-		description: '',
+		school: '國立臺中教育大學',
+		time: '2024/09 - 現在',
+		degree: '碩士',
+		location: '台中市, 台灣',
+		description: '數學教育所。',
+	},
+	{
+		school: '國立清華大學',
+		time: '2016/09 - 2019/02',
+		degree: '碩士',
+		location: '新竹市, 台灣',
+		description: '應用數學所',
+	},
+	{
+		school: '國立新竹教育大學',
+		time: '2013/09 - 2016/06',
+		degree: '學士',
+		location: '新竹市, 台灣',
+		description: '應用數學系。',
 	},
 	// {
 	// 	school: 'University of Paris',
@@ -45,12 +59,12 @@ export const skills = [
 
 export const publications = [
 	{
-		title: '',
-		authors: '',
+		title: 'Deep Learning under various loss functions',
+		authors: 'Chung Liang Tsai',
 		journal: '',
-		time: '',
+		time: '2019',
 		link: '',
-		abstract: '',
+		abstract: 'The approach of this research is to the loss function cross entropy with Renyi entropy and Gini index of deep learning, and see is it still good at training and test of deep learning. First we\'d given a simulation data to compare four entropies; Shannon entropy, Renyi entropy, cross entropy and Gini index. The Renyi entropy and cross entropy are performed well in our simulation dataset.Next step,take Renyi entropy and Gini index as loss function compare with cross entropy, which is deep learning default usual loss function. The following are the main research contributions of this dissertation. The propose Renyi entropy is slightly better than cross entropy.',
 	},
 	// {
 	// 	title: 'The Radiation of Uranium Compounds',
